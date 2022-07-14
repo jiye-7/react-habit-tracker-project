5feea8f424481318bd9cf551103f4409cd2ea637
jiye-7 <jy522hi@gmail.com>
2022-07-14 13:36:01 +0900

    app 컴포넌트의 모든 로직을 presenter로 분리

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app';
 import './index.css';
 import '@fortawesome/fontawesome-free/js/all.js';
+import HabitPresenter from './habit_presenter';
+
+const habitPresenter = new HabitPresenter([
+  { id: 1, name: 'Reading', count: 0 },
+  { id: 2, name: 'Running', count: 0 },
+  { id: 3, name: 'Coding', count: 0 },
+]);
 
 const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(
   <React.StrictMode>
-    <App />
+    <App presenter={habitPresenter} />
   </React.StrictMode>
 );
