f55fd48da7ca6c74e37480a1e1397384476a93bd
jiye-7 <jy522hi@gmail.com>
2022-07-15 15:22:46 +0900

    spanshot test 구현

--- src/components/form/test/habitAddForm.test.js
+++ src/components/form/test/habitAddForm.test.js
@@ -2,37 +2,50 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import renderer from 'react-test-renderer';
 import HabitAddForm from '../habitAddForm';
 
 describe('HabitAddForm', () => {
-  let onAdd;
-  let input;
-  let button;
-
-  const setup = () => {
-    render(<HabitAddForm onAdd={onAdd} />);
-  };
-
-  beforeEach(() => {
-    onAdd = jest.fn(); // jest의 mock함수를 이용
-    setup();
-    input = screen.getByPlaceholderText('Habit');
-    button = screen.getByText('Add');
+  /** 컴포넌트가 정상적으로 렌더링되는지 스냅샷테스트를 통해 확인 */
+  it('render', () => {
+    // 스냅샷 테스트 실행
+    // 컴포넌트 만들기
+    const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />);
+    expect(component.toJSON()).toMatchSnapshot();
   });
 
-  // onAdd 함수는 버튼이 클릭되면 호출한다. habit은 유효한 값이어야 된다.
-  it('calls onAdd when button is clicked and valid habit is entered', () => {
-    userEvent.type(input, 'New Habit');
-    userEvent.click(button);
+  /** Form이 제대로 Submit되는지에 대한 테스트는 별도로 분리 */
+  describe('Form Submit', () => {
+    let onAdd;
+    let input;
+    let button;
 
-    expect(onAdd).toHaveBeenCalledWith('New Habit'); // onAdd함수가 입력한 'New Habit'과 함께 호출되어야 한다.
-  });
+    /*  const setup = () => {
+      render(<HabitAddForm onAdd={onAdd} />);
+    };
+ */
+    beforeEach(() => {
+      onAdd = jest.fn(); // jest의 mock함수를 이용
+      // setup();
+      render(<HabitAddForm onAdd={onAdd} />);
+      input = screen.getByPlaceholderText('Habit');
+      button = screen.getByText('Add');
+    });
 
-  it('does not call onAdd when the habit is empty', () => {
-    userEvent.type(input, '');
-    userEvent.click(button);
+    // onAdd 함수는 버튼이 클릭되면 호출한다. habit은 유효한 값이어야 된다.
+    it('calls onAdd when button is clicked and valid habit is entered', () => {
+      userEvent.type(input, 'new habit');
+      userEvent.click(button);
 
-    // expect(onAdd).toHaveBeenCalledWith(''); // 문자열이 비어있으면 호출하면 안되니까 callTimes로 0번 호출되었는지를 검사하면 된다.
-    expect(onAdd).toHaveBeenCalledTimes(0);
+      expect(onAdd).toHaveBeenCalledWith('new habit'); // onAdd함수가 입력한 'New Habit'과 함께 호출되어야 한다.
+    });
+
+    it('does not call onAdd when the habit is empty', () => {
+      userEvent.type(input, '');
+      userEvent.click(button);
+
+      // expect(onAdd).toHaveBeenCalledWith(''); // 문자열이 비어있으면 호출하면 안되니까 callTimes로 0번 호출되었는지를 검사하면 된다.
+      expect(onAdd).toHaveBeenCalledTimes(0);
+    });
   });
 });
